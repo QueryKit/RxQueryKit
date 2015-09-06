ef126b80d99462dbc5bb71554a5cc0161c890d84
Kyle Fuller <kyle@fuller.li>
2015-09-05 22:22:22 -0700

    Initial project

---
+++ RxQueryKit/RxQueryKit.h
@@ -0,0 +1,19 @@
+//
+//  RxQueryKit.h
+//  RxQueryKit
+//
+//  Created by Kyle Fuller on 05/09/2015.
+//  Copyright © 2015 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxQueryKit.
+FOUNDATION_EXPORT double RxQueryKitVersionNumber;
+
+//! Project version string for RxQueryKit.
+FOUNDATION_EXPORT const unsigned char RxQueryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxQueryKit/PublicHeader.h>
+
+
